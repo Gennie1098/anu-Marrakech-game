6b067f3e619ef4c484ca5aaa6af99d6dcf0e0fc3
tongyuanxiong <u7733153@anu.edu.au>
2023-08-23 14:01:16 +0800

    complete the part

---
+++ src/comp1110/ass2/Angle.java
@@ -0,0 +1,41 @@
+package comp1110.ass2;
+
+public class Angle{
+    public int angle[];
+    int right = 90;
+    int back  = 180;
+    int left = 270;,
+    int front =360;
+
+//    this is the array contain the angle
+/** This is about the rotation angle of the Assam
+ *
+ *
+ *
+ */
+
+    /**
+     * This is function of to get the angle from the value
+     * Convert value in degrees into a cardinal direction
+     *
+     * @return new cardinal direction if angle divisible by 90, else null
+     */
+public static int getAngleFromValue(int value){
+
+    return value;
+
+
+}
+    /**
+     * Converts the angle to radians
+     *@return angle in radians
+     *
+     *
+     */
+    public void getRad() {
+        return ;
+    }
+
+
+
+}
---
+++ src/comp1110/ass2/Score.java
@@ -0,0 +1,55 @@
+package comp1110.ass2;
+
+public class Score {
+    int playerScore;
+    /**
+     * update the state of the playstate
+     *  For this method, you should update the player's status
+     *  First， you should find the playerState using a String,
+     * then, update the number of Rugs or Dirhmas
+     *     return a String that about the updated player
+     *
+     *
+     */
+    public static int calScore(int numberofRugs){
+        // implement the function
+        return numberofRugs;
+    }
+
+    /**
+     * compare all players’ score
+     * if someone's dirmas is not  equal to another, then you should use this function
+     * and notice you should compare the total Score.
+     * return a String who wins the game, just like Pr00803i
+     *
+     *
+     */
+    public static int  compareScores(int Score){
+        // implement the function
+        return  Score;
+    }
+
+    /**
+     * calculate player’s dirhmas
+     * if someone's dirmas is equal to another, then you should use this function
+     * and notice you should just compare the numbers of dirhmas instead of the score
+     *
+     *
+     */
+    public static int compareDirhmas(int amountofDirhmas){
+        // implement the function
+        return amountofDirhmas;
+    }
+    /**
+     * Get the final Result
+     * if someone wins the game,the gameResult will be display
+     * notice the winner should be like this (winner + players’ score from high to low)
+     * and all the player should be display
+     *
+     *
+     */
+    public static void  gameResult(){
+
+    }
+
+}
